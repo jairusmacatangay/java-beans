9845bf56bb65e4636fcb41538046407455e801fc
Gabriel Garcia <Gabriel Garcia@DESKTOP-FMSR09H>
2020-04-24 21:36:05 +0800

    I commit

--- src/com/javabeans/products/Products.java
+++ src/com/javabeans/products/Products.java
@@ -1,5 +1,88 @@
 package com.javabeans.products;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
 public class Products {
 
+	private int id;
+	private String name;
+	private String category;
+	private float price;
+	private String description;
+	//private image
+	private Date datetime;
+	
+	List<String> categoryOptions;
+	
+	public Products()
+	{
+		
+	}
+	
+	public Products(int id, String name, String category, float price, String description)
+	{
+		this.id = id;
+		this.name = name;
+		this.category = category;
+		this.price = price;
+		this.description = description;
+	}
+	
+	public List<String> getCategoryOptions() {
+		return categoryOptions;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDatetime() {
+		return datetime;
+	}
+
+	public void setDatetime(Date datetime) {
+		this.datetime = datetime;
+	}
+	
 }
--- src/com/javabeans/products/ProductsDBUtil.java
+++ src/com/javabeans/products/ProductsDBUtil.java
@@ -1,5 +1,166 @@
 package com.javabeans.products;
 
-public class ProductsDBUtil {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.http.Part;
+import javax.sql.DataSource;
+
+
+public class ProductsDBUtil {
+	
+	private static ProductsDBUtil instance;
+	private DataSource dataSource;
+	private String jndiName = "java:comp/env/jdbc/java_beans_db";
+	
+	public static ProductsDBUtil getInstance() throws Exception
+	{
+		if(instance == null)
+		{
+			instance = new ProductsDBUtil();
+		}
+		
+		return instance;
+	}
+	
+	private ProductsDBUtil() throws Exception
+	{
+		dataSource = getDataSource();
+	}
+	
+	private DataSource getDataSource() throws NamingException
+	{
+		Context context = new InitialContext();
+		DataSource theDataSource = (DataSource) context.lookup(jndiName);
+		return theDataSource;
+	}
+	
+	public List<Products> getProducts() throws Exception
+	{
+		List<Products> products = new ArrayList<>();
+		
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+		
+		try
+		{
+			myConn = getConnection();
+			String sql = "select product_id, product_category, product_name, description, price from products";
+			
+			myStmt = myConn.createStatement();
+			myRs = myStmt.executeQuery(sql);
+			
+			//process results
+			while (myRs.next())
+			{
+				//get data from result set row
+				int id = myRs.getInt("product_id");
+				String category = myRs.getString("product_category");
+				String name = myRs.getString("product_name");
+				String description = myRs.getString("description");
+				float price = myRs.getFloat("price");
+				
+				//create product object
+				Products tempProduct = new Products(id, name, category, price, description);
+				
+				//add it to the list of products
+				products.add(tempProduct);
+			}
+		}
+		finally
+		{
+			//clean up JDBC objects
+			close(myConn, myStmt, myRs);
+		}
+		
+		return products;
+	}
+	
+	public void addProduct(Products theProduct) throws Exception {
+		
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+		
+		try {
+			myConn = getConnection();
+			
+			String sql = "insert into products (product_category, product_name, description, price) values (?, ?, ?, ?)";
+			
+			myStmt = myConn.prepareStatement(sql);
+			
+			//set params
+			myStmt.setString(1, theProduct.getCategory());
+			myStmt.setString(2, theProduct.getName());
+			myStmt.setString(3, theProduct.getDescription());
+			myStmt.setFloat(4, theProduct.getPrice()); 
+			//myStmt.setString(5, datetime);
+			
+			myStmt.execute();
+		}
+		finally 
+		{
+			//close(myConn, myStmt);
+		}
+		
+	}
+	
+	public void deleteProduct(int productId) throws Exception {
+
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+
+		try {
+			myConn = getConnection();
+
+			String sql = "delete from products where product_id = ?";
+
+			myStmt = myConn.prepareStatement(sql);
+
+			// set params
+			myStmt.setInt(1, productId);
+			
+			myStmt.execute();
+		}
+		finally 
+		{
+			//close (myConn, myStmt);
+		}		
+	}	
+	
+	private Connection getConnection() throws Exception {
+
+		Connection theConn = dataSource.getConnection();
+		
+		return theConn;
+	}
+	
+	private void close(Connection theConn, Statement theStmt, ResultSet theRs) {
+
+		try {
+			if (theRs != null) {
+				theRs.close();
+			}
+
+			if (theStmt != null) {
+				theStmt.close();
+			}
+
+			if (theConn != null) {
+				theConn.close();
+			}
+			
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+	
 }
