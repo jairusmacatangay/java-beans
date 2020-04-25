0d2c6b3b52c6545f5b369e5208b6d8cf37e7121b
Jairus Macatangay <jairus.macatangay@benilde.edu.ph>
2020-04-25 19:58:17 +0800

    Updated back-end for account-details, edit-account details, edit-account-details, register

--- src/com/javabeans/users/UsersController.java
+++ src/com/javabeans/users/UsersController.java
@@ -45,15 +45,21 @@ public class UsersController {
 	
 	//login
 	public String validateUser(Users theUser) throws Exception {
-		
-		theUser = usersDBUtil.validateUser(theUser);
-		
-		if(theUser != null) {
-			return "/pages/customer/home-page";
-		} else {
-			FacesContext.getCurrentInstance().addMessage("login:submit", new FacesMessage("Incorrect Email and Password!"));
-			return "login";
+		try {
+			theUser = usersDBUtil.validateUser(theUser);
+			String user_type = theUser.getUser_type();
+			
+			if(user_type.equals("Customer")) {
+				return "/pages/customer/home-page?faces-redirect=true";
+			} else if (user_type.equals("Admin")) {
+				return "/pages/admin/dashboard?faces-redirect=true";
+			}
+		} catch (Exception ex) {
+			addErrorMessage(ex);
 		}
+		
+		FacesContext.getCurrentInstance().addMessage("login:submit", new FacesMessage("Incorrect Email and Password!"));
+		return "login";
 	}
 	
 	//get account details for view
@@ -93,6 +99,19 @@ public class UsersController {
 		return "edit-account-details?faces-redirect=true";
 	}
 	
+	public String editAccountDetailsForCheckout(Users theUser) {
+		try {
+			theUser = usersDBUtil.getUserDetailsForCheckout(theUser);
+			ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+			Map<String, Object> requestMap = externalContext.getRequestMap();
+			requestMap.put("users", theUser);
+		} catch (Exception ex) {
+			addErrorMessage(ex);
+			return null;
+		}
+		return "edit-account-details-checkout?faces-redirect=true";
+	}
+	
 	//update account details
 	public String updateAccountDetails(Users theUser) {
 		logger.info("Update account details for user: " + theUser);
@@ -105,7 +124,17 @@ public class UsersController {
 			return null;
 		}
 		
-		return "/pages/customer/home-page";
+		return "account-details?faces-redirect=true";
+	}
+	
+	public String updateAccountDetailsForCheckout(Users theUser) {
+		try {
+			usersDBUtil.updateUserDetailsForCheckout(theUser);
+		} catch (Exception ex) {
+			addErrorMessage(ex);
+			return null;
+		}
+		return "checkout?faces-redirect=true";
 	}
 	
 	//Transaction History List
