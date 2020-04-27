d8b43b686b3ede2df868759bdfafd89797693a5d
Gabriel Garcia <Gabriel Garcia@DESKTOP-FMSR09H>
2020-04-27 14:50:19 +0800

    updated users

--- src/com/javabeans/users/Users.java
+++ src/com/javabeans/users/Users.java
@@ -1,5 +1,60 @@
 package com.javabeans.users;
 
 public class Users {
+	
+	private String firstName;
+	private String lastName;
+	private String city;
+	private String gender;
+	//private String dob;
+	
+	public Users(String firstName, String lastName, String city, String gender)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.city = city;
+		this.gender = gender;
+		//this.dob = dob;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	/*public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}*/
 
 }
--- src/com/javabeans/users/UsersController.java
+++ src/com/javabeans/users/UsersController.java
@@ -1,5 +1,52 @@
 package com.javabeans.users;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import com.javabeans.users.UsersDBUtil;
+
+@ManagedBean
+@SessionScoped
 public class UsersController {
+	
+	private List<Users> users;
+	private UsersDBUtil usersDBUtil;
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	public UsersController() throws Exception {
+		users = new ArrayList<>();
+		
+		usersDBUtil = UsersDBUtil.getInstance();
+	}
+	
+	public List<Users> getUsers() {
+		return users;
+	}
+	
+	public void loadUsers()
+	{
+		logger.info("Loading Users");
+		users.clear();
+		
+		try
+		{
+              users = usersDBUtil.getUsers();
+		}
+		catch(Exception ex)
+		{
+			addErrorMessage(ex);
+		}
+	}
+	
+	private void addErrorMessage(Exception exc) {
+		FacesMessage message = new FacesMessage("Error: " + exc.getMessage());
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
 
 }
